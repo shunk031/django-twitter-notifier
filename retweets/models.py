a19945fba23d0049ed730ae9e78cfcd6aaaea9b5
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-03-07 23:04:39 +0900

    update models

--- retweets/models.py
+++ retweets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class RetweetTweet(models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+    tweet_id = models.CharField(max_length=255, unique=True)
+    tweet = models.CharField(max_length=300)
+    user_id = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=255)
+    user_screen_name = models.CharField(max_length=255)
+    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    favorite_count = models.IntegerField(default=0)
+    retweet_count = models.IntegerField(default=0)
+
+    class Meta:
+        managed = False
+        db_table = 'twitter_retweets'
