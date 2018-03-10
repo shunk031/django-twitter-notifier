e46d004d267f0a2c3762192d78676662d96415aa
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-03-10 15:03:51 +0900

    update notifyslack command

--- twitternotifier/management/commands/notifyslack.py
+++ twitternotifier/management/commands/notifyslack.py
@@ -1,36 +1,47 @@
+import random
 import sys
+import time
+
 import slack
+import slack.chat
 
 from django.core.management.base import BaseCommand
 
 from twitternotifier.logging import get_logger
 from twitternotifier.static_settings import STATIC_SETTINGS
+from favorites.models import FavoriteTweet
 
 logger = get_logger(__name__)
+
+NUM_OF_NOTIFY = 10
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        pass
+        try:
+            entries = FavoriteTweet.objects.order_by('?')[:NUM_OF_NOTIFY]
+
+            for entry in entries:
+                _to_notify(room=STATIC_SETTINGS['SLACK_NOTIFICATION_ROOM_ID'],
+                           message=entry.original_url,
+                           icon_emoji=':heart:')
+                time.sleep(5)
+        except Exception as err:
+            logger.exception('[{}] {}'.format(
+                sys._getframe().f_code.co_name, err))
 
 
-def _to_notify(room, message, title='', author='', color='', icon_emoji=''):
+def _to_notify(room, message, icon_emoji=''):
     slack.api_token = STATIC_SETTINGS['SLACK_TOKEN']
-
-    attachments = [{
-        'title': title,
-        'fallback': message,
-        'color': color,
-        'author_name': author,
-        'icon_emoji': icon_emoji,
-        'text': message,
-        'html': ['text', 'pretext', 'title'],
-    }]
 
     try:
         slack.chat.post_message(
-            room, '', username=author, attachments=attachments, icon_emoji=icon_emoji)
+            room,
+            message,
+            username='Favorite Notification Man',
+            icon_emoji=icon_emoji)
+
     except slack.exception.NotAuthedError as err:
         logger.exception('[{}] {}'.format(
             sys._getframe().f_code.co_name, err))
