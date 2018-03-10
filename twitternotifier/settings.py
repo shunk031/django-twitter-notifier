4aec0e37d178964aca026d59484ce5069dde1093
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-03-10 15:03:26 +0900

    DEBUG = True

--- twitternotifier/settings.py
+++ twitternotifier/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0votov)&5&0_*qietd6x-0f)0wptv#oqu@y_hpfltg@ot*pq6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
