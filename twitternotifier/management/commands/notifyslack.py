56c2b659f805ba67e9fb0bd8f4932ad732baf4aa
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-05-20 01:08:46 +0900

    remove protected user entries and deleted tweets when notify to slack

--- twitternotifier/management/commands/notifyslack.py
+++ twitternotifier/management/commands/notifyslack.py
@@ -4,23 +4,32 @@ import time
 
 import slack
 import slack.chat
+import tweepy
 
 from django.core.management.base import BaseCommand
 
+from twitternotifier.api.twitter import twitterapi
 from twitternotifier.logging import get_logger
 from twitternotifier.static_settings import STATIC_SETTINGS
 from favorites.models import FavoriteTweet
 
 logger = get_logger(__name__)
 
-NUM_OF_NOTIFY = 10
-
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            entries = FavoriteTweet.objects.order_by('?')[:NUM_OF_NOTIFY]
+            entries = FavoriteTweet.objects.order_by('?')[:STATIC_SETTINGS['NUM_OF_NOTIFY']]
+            while True:
+                entries = _remove_protected_user_entry(entries)
+                entries = _remove_deleted_tweets(entries)
+                entries = list(set(entries))  # remove duplicates
+
+                if len(entries) >= STATIC_SETTINGS['NUM_OF_NOTIFY']:
+                    break
+
+                entries += FavoriteTweet.objects.order_by('?')[:STATIC_SETTINGS['NUM_OF_NOTIFY'] - len(entries)]
 
             for entry in entries:
                 _to_notify(room=STATIC_SETTINGS['SLACK_NOTIFICATION_ROOM_ID'],
@@ -30,6 +39,36 @@ class Command(BaseCommand):
         except Exception as err:
             logger.exception('[{}] {}'.format(
                 sys._getframe().f_code.co_name, err))
+
+
+def _remove_protected_user_entry(entries):
+
+    new_entries = []
+    for entry in entries:
+        try:
+            is_protected = twitterapi().get_user(entry.user_id).protected
+
+            if not is_protected:
+                new_entries.append(entry)
+        except tweepy.error.TweepError as err:
+            logger.exception('[{}] {}'.format(
+                sys._getframe().f_code.co_name, err))
+
+    return new_entries
+
+
+def _remove_deleted_tweets(entries):
+
+    entry_ids = [entry.tweet_id for entry in entries]
+    lookup_result = twitterapi().statuses_lookup(entry_ids)
+    lookup_entry_ids = set([result.id_str for result in lookup_result])
+
+    new_entries = []
+    for entry in entries:
+        if entry.tweet_id in lookup_entry_ids:
+            new_entries.append(entry)
+
+    return new_entries
 
 
 def _to_notify(room, message, icon_emoji=''):
